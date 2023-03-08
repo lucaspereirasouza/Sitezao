f18c4d1391db672ae34e73aada8bc9ae6aae5fd2
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-03-08 17:06:15 -0300

    Adicionado conteudo no index e no javascript

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-document.getElementById("bttn").onclick = function{
-    document.getElementById("gif")
+let clicado = false;
+
+document.getElementById("botaoramos").onclick = function(){  
+  if (clicado == false){
+  clicado = true
+    var img = document.getElementById("imagem")
+  img.style.width = "500px";
+  return img;
+  }
+  else{
+    clicado = false
+    var img = document.getElementById("imagem")
+  img.style.width = "35px";
+  return img;
+  }
 }
