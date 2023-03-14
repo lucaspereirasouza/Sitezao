c7ed2b007b266860b7c837f0541348c0281f73d8
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-03-14 17:28:28 -0300

    Modificação de arquivos

--- bagunca/index.js
+++ bagunca/index.js
@@ -1,1 +1,9 @@
 let button = document.getElementById("bttn01")
+
+button.onclick = function start() {
+  document.getElementById('logo').style.animation = "anim 2s 2s forward";
+  document.getElementById('earthlogo').style.animation = "anim2 2s 2s forward";
+}
+
+
+document.getElementsByClassName("")
