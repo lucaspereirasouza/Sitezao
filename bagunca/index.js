08b8cc9143a181b60a29a2c7f766178d4cd55659
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-03-14 15:10:44 -0300

    adicionando icone no site

--- bagunca/index.js
+++ bagunca/index.js
@@ -1,16 +1,1 @@
-let clicado = false;
-
-document.getElementById("botaoramos").onclick = function(){  
-  if (clicado == false){
-  clicado = true
-    var img = document.getElementById("imagem")
-  img.style.width = "35px";
-  return img;
-  }
-  else{
-    clicado = false
-    var img = document.getElementById("imagem")
-  img.style.width = "500px";
-  return img;
-  }
-}
+let button = document.getElementById("bttn01")
